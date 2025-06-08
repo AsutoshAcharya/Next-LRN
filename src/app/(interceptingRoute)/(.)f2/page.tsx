3ffe52bfdae25be7b1a967f3fbc7be83c16af80e
Asutosh Acharya <asutosh246@gmail.com>
2025-06-08 17:58:18 +0530

    intercepting routes

---
+++ src/app/(interceptingRoute)/(.)f2/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const F2Intercepted = () => {
+  return <div>F2Intercepted page</div>;
+};
+
+export default F2Intercepted;
---
+++ src/app/(interceptingRoute)/f1/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const F1 = () => {
+  return (
+    <div>
+      F1 page
+      <Link href="/f2">To F2</Link>
+    </div>
+  );
+};
+
+export default F1;
